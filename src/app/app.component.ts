6a41ab9483f2cb2c16f92c038c877cd273b0282a
Armando <armando.roman.jr20@gmail.com>
2021-01-03 23:00:33 -0600

    change detection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,19 @@ import {FavoriteChangedArgs} from './favorite/favorite.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  viewMode = 'map';
-  }
+courses = [
+  { id: 1, name: 'course1 '},
+  { id: 2, name: 'course2 '},
+  { id: 3, name: 'course3 '},
+];
+
+onAdd() {
+  this.courses.push({id: 4, name: 'course4' });
+}
+
+onChange(course) {
+  course.name = 'UPDATED';
+}
+}
 
 
